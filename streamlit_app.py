e0c16918e6d7a7e9459e69299833b933b1b80d20
lyios <muichiro2104@gmail.com>
2024-07-16 08:31:55 +0000

    ｆｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,16 +49,29 @@ if 'selected_word' in st.session_state:
     if st.session_state.display_meaning:
         st.write(f"日本語訳: {st.session_state.selected_word['日本語訳']}")
 
-    if st.button('英文を見る'):
-        st.session_state.display_meaning = True
+   if st.button('意味を確認する'):
+    st.session_state.display_meaning = 'meaning'
 
-    if st.session_state.display_meaning:
+if st.session_state.display_meaning == 'meaning':
+    if '日本語訳' in st.session_state.selected_word:
+        st.write(f"日本語訳: {st.session_state.selected_word['日本語訳']}")
+    else:
+        st.write("日本語訳がありません")
+
+if st.button('英文を見る'):
+    st.session_state.display_meaning = 'english'
+
+if st.session_state.display_meaning == 'english':
+    if '英文' in st.session_state.selected_word:
         st.write(f"英文: {st.session_state.selected_word['英文']}")
+    else:
+        st.write("英文がありません")
 
-    if st.button('日本文を見る'):
-        st.session_state.display_meaning = True
+if st.button('日本文を見る'):
+    st.session_state.display_meaning = 'japanese'
 
-    if st.session_state.display_meaning:
+if st.session_state.display_meaning == 'japanese':
+    if '日本文' in st.session_state.selected_word:
         st.write(f"日本文: {st.session_state.selected_word['日本文']}")
-
-    
+    else:
+        st.write("日本文がありません")
