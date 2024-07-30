1e230b43d66f3c364195b537c32165c6dc60f181
lyios <muichiro2104@gmail.com>
2024-07-30 03:16:53 +0000

    ｊもｐｊ

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,6 @@ def draw_gacha():
 def main():
         st.header("英検準二級英単語ガチャ")
 
-        if st.session_state.quest_completed:
-            draw_gacha()
-
     # Load the data
         @st.cache
         def load_data():
