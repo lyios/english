ab23aa49d3c2c7572852e62ac98cf5796597a21f
lyios <muichiro2104@gmail.com>
2024-07-31 02:58:32 +0000

    え６ｋ

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,22 +106,26 @@ def question():
     if st.button('選択肢を表示する'):
             st.session_state.show_options = True
 
-    if st.session_state.show_options:  
-        user_answer = st.radio(
-        "この単語の品詞は？",
-        options=options,
-        key="quiz_radio"
-    )
+    if st.session_state.show_options:
+        if word and correct_pos:  
+            user_answer = st.radio(
+            "この単語の品詞は？",
+            options=options,
+            key="quiz_radio"
+        )
 
     # 回答のチェックとフィードバックを表示
-    if user_answer == correct_pos:
-        st.success("正解です！")
-        st.session_state.quest_completed = True
-        st.session_state.selected_word = {'単語': word, '品詞': correct_pos}
-        draw_gacha()
+        if user_answer == correct_pos:
+            st.success("正解です！")
+            st.session_state.quest_completed = True
+            st.session_state.selected_word = {'単語': word, '品詞': correct_pos}
+            draw_gacha()
         
+        else:
+            st.error(f"不正解です。正解は「{correct_pos}」です。")
+
     else:
-        st.error(f"不正解です。正解は「{correct_pos}」です。")
+                st.error("単語が選ばれていないため、選択肢が表示できません。")
 
 
 def main():
